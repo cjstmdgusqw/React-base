f6aa4d05acd9d2615023ccf4a880d31d464c96dc
cjstmdgusqw <cjstmdgusqw@gmail.com>
2023-04-10 10:49:33 +0900

    React class

---
+++ classtest/classtest3.js
@@ -0,0 +1,25 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    info(){
+        return `이름 : ${this.name}, 나이 : ${this.age}`
+    }
+}
+
+class Student extends Person {
+    constructor(name,age,subject){
+        super(name, age);
+        this.subjubt = subject;
+    }
+    info(){
+        return `이름 : ${this.name}, 나이 : ${this.age}, 학과 : ${this.subject}`
+    }
+}
+
+let person = new Person('hong', 20);
+console.log(person.info());
+
+let student = new Student('park', 20);
+console.log(student.info());
