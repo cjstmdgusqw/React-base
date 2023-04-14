c3283d44a34996a03e2bc1321d03f5f02c78c408
cjstmdgusqw <cjstmdgusqw@gmail.com>
2023-04-14 10:06:10 +0900

    react day2 and day3

--- protest4/src/App.js
+++ protest4/src/App.js
@@ -6,7 +6,7 @@ import Employee from './Employee';
 
 function App() {
   let department = [{id : 10, name : '홍보부'},{id : 11, name : '개발부'}, {id : 12, name : '영업부'}];
-   let employee = [{id : 100, name : '홍길동', dept : 10},{id : 101, name : '김길동', dept : 20},{id : 102, name : '고길동', dept : 30}]
+  let employee = [{id : 100, name : '홍길동', dept : 10},{id : 101, name : '김길동', dept : 20},{id : 102, name : '고길동', dept : 30}]
   return (
     <div>
       <div>
